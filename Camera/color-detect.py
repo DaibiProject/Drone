8d946492e2fb8f8a8a7662d415f993fb1b2742d9
MorningStar3600 <MorningStar3600@users.noreply.github.com>
2020-02-19 18:34:42 +0100

    FPS display

--- Camera/color-detect.py
+++ Camera/color-detect.py
@@ -7,6 +7,8 @@ while True:
     _, frame = cap.read()
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+    tickmark=cv2.getTickCount()
+    
     #Wanted color
     low_c = np.array([161, 155, 84])
     high_c = np.array([179, 255, 255])
@@ -16,10 +18,16 @@ while True:
     high_d = np.array([240, 100, 80])
     mask2 = cv2.inRange(hsv_frame, low_d, high_d)
     
-    cv2.imshow("Frame", frame)
     cv2.imshow("Frame2", mask)
     cv2.imshow("Frame3", mask2)
 
+    
+    fps=cv2.getTickFrequency()/(cv2.getTickCount()-tickmark)
+    cv2.putText(frame, "FPS: {:05.2f}".format(fps), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+    cv2.imshow('video', frame)
+    
     key = cv2.waitKey(1)
     if key == 27:
         break
+
+    
