e4047eb0d3a5ab99ddad6fb8e54ed71a993a3f7c
MorningStar3600 <MorningStar3600@users.noreply.github.com>
2020-02-19 18:24:52 +0100

    Color detection
    Detect and display red and blue color on an separated mask.

--- Camera/color-detect.py
+++ Camera/color-detect.py
@@ -5,7 +5,20 @@ cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    #Wanted color
+    low_c = np.array([161, 155, 84])
+    high_c = np.array([179, 255, 255])
+    mask = cv2.inRange(hsv_frame, low_c, high_c)
+    
+    low_d = np.array([240, 100, 50])
+    high_d = np.array([240, 100, 80])
+    mask2 = cv2.inRange(hsv_frame, low_d, high_d)
+    
     cv2.imshow("Frame", frame)
+    cv2.imshow("Frame2", mask)
+    cv2.imshow("Frame3", mask2)
 
     key = cv2.waitKey(1)
     if key == 27:
