675d3b446610b403c4e91ed15dab00573ac7a0ef
MorningStar3600 <MorningStar3600@users.noreply.github.com>
2020-02-20 19:20:14 +0100

    Tests
    Added contour point, and it should display on a separate image, when stop (escape).
    Still working on it

--- Camera/color-detect.py
+++ Camera/color-detect.py
@@ -14,16 +14,14 @@ while True:
     high_c = np.array([179, 255, 255])
     mask = cv2.inRange(hsv_frame, low_c, high_c)
     
-    low_d = np.array([94, 80, 2])
+    low_d = np.array([94, 120, 80])
     high_d = np.array([126, 255, 255])
     mask2 = cv2.inRange(hsv_frame, low_d, high_d)
-
-    bilateral = cv2.medianBlur(mask2,15)
     
+    contours, hierarchy = cv2.findContours(mask2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     #display Masks
     cv2.imshow("Frame2", mask)
     cv2.imshow("Frame3", mask2)
-    cv2.imshow('bilateral Blur',bilateral)
     
     fps=cv2.getTickFrequency()/(cv2.getTickCount()-tickmark)
     cv2.putText(frame, "FPS: {:05.2f}".format(fps), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
@@ -35,4 +33,12 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-    
+
+#should display contour point.
+print(contours)
+img = np.zeros((512,512,3), np.uint8)
+
+for cnt in contours:
+    cv2.circle(img, (cnt[[[tuple(0)]]],cnt[[[tuple(1)]]]), 10, (0,0,255), -1)
+
+cv2.imshow("test",img)
