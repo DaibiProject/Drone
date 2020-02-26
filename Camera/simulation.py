04b2b3a95014ac8554934531b574104c534b3ab3
MorningStar3600 <MorningStar3600@users.noreply.github.com>
2020-02-26 14:15:29 +0100

    Debug

--- Camera/simulation.py
+++ Camera/simulation.py
@@ -40,18 +40,18 @@ while True:
     cv2.circle(img, (x,y), 1, (0,0,255), 2)
     cv2.circle(img, (200,200), 16, (0,255,0), 2)
 
-    u = [200 - x,-200 + y]
+    u = [-200 - x,200 + y]
 
     cv2.line(img, (200,200), (int(x),int(y)), (255,0,0), 1)
     if u[0] == 0:
         u[0] = 0.00001
     v = u[1]/u[0]
-    print(v)
+    print(200, 200 +v*10)
 
-    cv2.line(img, (200,200), (int(v), int(v)), (255,0,0), 1)
+    cv2.line(img, (200,200), (210, (200 + int(v)*10)), (255,0,0), 1)
 
     
-
+    cv2.circle(img, (10, int(v*10)), 1, (0,255,0),1)
     fps=cv2.getTickFrequency()/(cv2.getTickCount()-tickmark)
     cv2.putText(img, "FPS: {:05.2f}".format(fps), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
     cv2.imshow("simu",img)
