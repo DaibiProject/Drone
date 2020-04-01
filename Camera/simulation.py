d3ca0d83925dd5ac24a33f01bce2b9a3b25876bf
MorningStar3600 <Alexandre.michaud.montrouge@gmail.com>
2020-04-01 10:14:12 +0200

    Error sphere

--- Camera/simulation.py
+++ Camera/simulation.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import math
-img = np.zeros((500,700,3), np.uint8)
+img = np.zeros((700,900,3), np.uint8)
 
 x=0.001
 y=0.001
@@ -9,14 +9,15 @@ m=0
 p=0
 xx=0
 yy=0
-
+rayon=0
 while True:
     tickmark=cv2.getTickCount()
     key = cv2.waitKey(1)
     if key == 27:
         break
     cv2.line(img, (int(x),int(y)), (int(xx)+200, int(yy)+200), (0,0,0), 1)
-    cv2.line(img, (int(x),int(y)), (-int(xx)+200, -int(yy)+200), (0,0,0), 1)
+    cv2.line(iqmg, (int(x),int(y)), (-int(xx)+200, -int(yy)+200), (0,0,0), 1)
+    cv2.circle(img, (int(x),int(y)), int(rayon), (0,0,0), -1)
     #if key != -1:
     #   print(key)
     if key == 122:
@@ -43,9 +44,11 @@ while True:
         print(fps)
     cv2.circle(img, (int(x),int(y)), 1, (0,0,255), 2)
     cv2.circle(img, (200,200), 16, (150,0,150), 2)
-
+    
+    
     cv2.line(img, (200,200), (int(xx)+200,int(yy)+200), (0,0,0), 1)
     cv2.line(img, (200,200), (200-int(xx),200-int(yy)), (0,0,0), 1)
+    
 
     
 
@@ -55,6 +58,14 @@ while True:
     
     xx = 16*math.cos(math.atan(m))
     yy = 16*math.sin(math.atan(m))
+
+    dist = math.sqrt((200-x)**2+(200-y)**2)
+    
+    rayon = 1.1825**(0.0378*dist)
+    print(rayon, int(rayon))
+    cv2.circle(img, (int(x),int(y)), int(rayon), (0,0,255), -1)
+    
+    
     
     cv2.line(img, (int(x),int(y)), (int(xx)+200, int(yy)+200), (0,200,0), 1)
     cv2.line(img, (int(x),int(y)), (-int(xx)+200, -int(yy)+200), (0,200,0), 1)
@@ -68,3 +79,4 @@ while True:
     cv2.putText(img, "FPS: {:05.2f}".format(fps), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
     
 cv2.destroyAllWindows()
+
