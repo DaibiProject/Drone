a4939722961981c114c551fad83e6b23b756a0d0
MorningStar3600 <Alexandre.michaud.montrouge@gmail.com>
2020-04-16 12:25:41 +0200

    Pathfinding - Obstacle Object

--- Camera/simulation.py
+++ Camera/simulation.py
@@ -14,6 +14,10 @@ dist=0
 x1=0
 y1=0
 obst= []
+taille_case = 20
+cx = 0
+cy = 0
+
 def mouse_drawing(event, x, y, flags, params):
     global obst
     
@@ -21,12 +25,52 @@ def mouse_drawing(event, x, y, flags, params):
         cv2.circle(img, (int(x), int(y)), 7, (255,255,255), 1)
         obst.append([x,y])
 
+class Obstacle:
+    global taille_case
+    obst = []
+    index = 0
+    def __init__(self):
+        self.x=0
+        self.y=0
+        self.index = index
+        index += 1
+        obst.append([x,y])
+
+        
+
+    def set_value(self, xx,yy):
+        self.x = xx
+        self.y = yy
+        self.x1 = xx/taille_case
+        self.y1 = yy/taille_case
+        obst[self.index]=[self.x,self.y]
+    
+
+def fill_case(x,y,cx,cy,color):
+   
+    cv2.rectangle(img, (cx,cy),(cx+taille_case,cy+taille_case),(0,0,0),-1)
+    cx = int(x/taille_case)*taille_case
+    cy = int(y/taille_case)*taille_case
+    cv2.rectangle(img, (cx,cy),(cx+taille_case,cy+taille_case),color,-1)
+    
 
 while True:
     tickmark=cv2.getTickCount()
     key = cv2.waitKey(1)
     if key == 27:
         break
+
+
+    i = 0
+    while i <900:
+        cv2.line(img, (int(i), 0), (int(i), 700), (30,30,30))
+        i+= taille_case
+    i = 0
+    while i < 700:
+        cv2.line(img, (0, int(i)), (900, int(i)), (30,30,30))
+        i+= taille_case
+
+        
     cv2.line(img, (int(x),int(y)), (int(xx)+200, int(yy)+200), (0,0,0), 1)
     cv2.line(img, (int(x),int(y)), (-int(xx)+200, -int(yy)+200), (0,0,0), 1)
     cv2.circle(img, (int(x),int(y)), int(rayon), (0,0,0), 1)
@@ -37,7 +81,8 @@ while True:
         
     for i in obst:
         cv2.line(img, (int(x),int(y)),(int(i[0]),int(i[1])), (0,0,0))
-    
+        fill_case(i[0],i[1],(50,20,20))
+    fill_case(x,y,cx,cy,(20,20,40))
     if key == 122:
         cv2.circle(img, (int(x),int(y)), 1, (0,0,0), 2)
         cv2.line(img, (200,200), (int(x),int(y)), (0,0,0), 1)
@@ -95,14 +140,7 @@ while True:
         cv2.circle(img, (int(i[0]),int(i[1])), 7, (255,255,255), 1)
         cv2.line(img, (int(x),int(y)),(int(i[0]),int(i[1])), (100,100,100))
 
-    i = 0
-    while i <900:
-        cv2.line(img, (int(i), 0), (int(i), 700), (30,30,30))
-        i+= 20
-    i = 0
-    while i < 700:
-        cv2.line(img, (0, int(i)), (900, int(i)), (30,30,30))
-        i+= 20
+    
         
         
 
